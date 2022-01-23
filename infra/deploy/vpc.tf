4d221322598e122edde373d2487bb70083c605ec
juanjhaus <juanbonilla@lahaus.com>
2022-01-23 15:35:11 -0500

    using eks module

--- infra/deploy/vpc.tf
+++ infra/deploy/vpc.tf
@@ -1,30 +1,141 @@
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "availability_zones" {
+  state = "available"
+}
 
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.2.0"
-
-  name                 = "education-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
+  tags = merge(local.tags, {
+    Name = "${var.prefix}-vpc"
+  })
+}
+
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.availability_zones.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(local.tags, {
+    Name = format("${var.prefix}-public%02d", count.index + 1)
+  })
+}
+
+
+resource "aws_subnet" "private_subnets" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.availability_zones.names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = merge(local.tags, {
+    Name = format("${var.prefix}-private%02d", count.index + 1)
+  })
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(local.tags, {
+    Name = "${var.prefix}-ig"
+  })
+}
+
+resource "aws_eip" "nat_gateway_elastic_ips" {
+  count = length(var.public_subnet_cidrs)
+  vpc   = true
+
+  tags = merge(local.tags, {
+    Name = format("${var.prefix}-eip%02d", count.index + 1)
+  })
+
+  depends_on = [aws_internet_gateway.internet_gateway]
+}
+
+resource "aws_nat_gateway" "nat_gateways" {
+  count         = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat_gateway_elastic_ips[count.index].id
+  subnet_id     = aws_subnet.public_subnets[count.index].id
+
+  tags = merge(local.tags, {
+    Name = format("${var.prefix}-ngw%02d", count.index + 1)
+  })
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = merge(local.tags, {
+    Name = "${var.prefix}-public"
+  })
+}
+
+resource "aws_route_table_association" "public_route_table_associations" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_tables" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateways[count.index].id
+  }
+
+  tags = merge(local.tags, {
+    Name = format("${var.prefix}-private%02d", count.index + 1)
+  })
+}
+
+resource "aws_route_table_association" "private_route_table_associations" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_tables[count.index].id
+}
+
+
+resource "aws_security_group" "no_ingress" {
+  name        = "${var.prefix}-no-ingress"
+  description = "No ingress traffic"
+  vpc_id      = aws_vpc.vpc.id
+
   tags = {
-    "kubernetes.io/cluster/${kubernetes_namespace.staging_ns.metadata.0.name}" = "shared"
+    Name = "${var.prefix}-no-ingress"
+  }
+}
+
+resource "aws_security_group" "mwaa" {
+  name        = "airflow-security-group-${var.prefix}"
+  description = "Security Group for Amazon MWAA Environment ${var.prefix}"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = -1
+    self      = true
   }
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${kubernetes_namespace.staging_ns.metadata.0.name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${kubernetes_namespace.staging_ns.metadata.0.name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
+  tags = merge(local.tags, {
+    Name = "airflow-security-group-${var.prefix}"
+  })
 }
