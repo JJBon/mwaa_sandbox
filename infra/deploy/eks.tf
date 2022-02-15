1b0271fad3e6a07ba26fa08be60e657dd4588d19
juanjhaus <juanbonilla@lahaus.com>
2022-02-14 19:45:25 -0500

    added thumbprint to openid provider

--- infra/deploy/eks.tf
+++ infra/deploy/eks.tf
@@ -99,9 +99,13 @@ resource "aws_eks_node_group" "nodes" {
   tags = local.tags
 }
 
+data "external" "thumb" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.cluster.identity.0.oidc.0.issuer]
+}
+
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = []
+  thumbprint_list = [data.external.thumb.result.thumbprint]
   url             = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
 }
 
