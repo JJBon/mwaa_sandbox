ebebbd04b368efd0a6f5e11ebae16001443124a6
juanjhaus <juanbonilla@lahaus.com>
2022-03-28 13:13:06 -0500

    restoring fargate profile

--- infra/deploy/k8s.tf
+++ infra/deploy/k8s.tf
@@ -126,34 +126,34 @@ resource "kubernetes_service_account" "mwaa_service_account" {
   }
 }
 
-# resource "aws_iam_role" "fargate_airflow_role" {
-#   name = "eks-fargate-profile-airflow"
+resource "aws_iam_role" "fargate_airflow_role" {
+  name = "eks-fargate-profile-airflow"
 
-#   assume_role_policy = jsonencode({
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "eks-fargate-pods.amazonaws.com"
-#       }
-#     }]
-#     Version = "2012-10-17"
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
 
 
-# resource "aws_iam_role_policy_attachment" "airflow-AmazonEKSFargatePodExecutionRolePolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-#   role       = aws_iam_role.fargate_airflow_role.name
-# }
+resource "aws_iam_role_policy_attachment" "airflow-AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_airflow_role.name
+}
 
-# resource "aws_eks_fargate_profile" "airflow_fargate" {
-#   cluster_name           = aws_eks_cluster.cluster.name
-#   fargate_profile_name   = "airflow"
-#   pod_execution_role_arn = aws_iam_role.fargate_airflow_role.arn
-#   subnet_ids             = [aws_subnet.private_subnets[0].id, aws_subnet.private_subnets[1].id]
+resource "aws_eks_fargate_profile" "airflow_fargate" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "airflow"
+  pod_execution_role_arn = aws_iam_role.fargate_airflow_role.arn
+  subnet_ids             = [aws_subnet.private_subnets[0].id, aws_subnet.private_subnets[1].id]
 
-#   selector {
-#     namespace = var.namespace_name
-#   }
-# }
+  selector {
+    namespace = var.namespace_name
+  }
+}
