547fd46c137882f48f5164299dacb915dfc0f540
juanjhaus <juanbonilla@lahaus.com>
2022-03-28 21:51:26 -0500

    Revert "explicit timeout"
    This reverts commit 4d32b1b10ef885850e510bfddb8e3b58ce964ed9.

--- airflow-docker/dags/utils/k8s_params.py
+++ airflow-docker/dags/utils/k8s_params.py
@@ -7,7 +7,6 @@ from airflow.kubernetes.secret import Secret
 from datetime import datetime, timedelta
 from kubernetes.client import models as k8s
 import os 
-from datetime import timedelta
 
 airflow_home = os.environ.get("AIRFLOW_HOME")
 
@@ -50,6 +49,5 @@ aws_args = {
     "is_delete_operator_pod": False,
     "configmaps":["airflow-vars"],
     "termination_grace_period":3000,
-    "resources":resources,
-    "execution_timeout": timedelta(minutes=30)
+    "resources":resources
 }
